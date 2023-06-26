7b51d16ab56c083cf922e016f13c6bb9a89734d9
Fombi-Favour <fombifavour@gmail.com>
2023-06-26 16:24:50 +0100

    Create seriesCard.js

---
+++ src/modules/seriesCard.js
@@ -0,0 +1,56 @@
+import { getSeries } from './getData.js';
+
+const seriesCard = async () => {
+//   const likes = await getLikes(); // Retrieve likes before updating series cards
+  const finalData = await getSeries();
+  const seriesCardWrapper = document.getElementById('series');
+  seriesCardWrapper.innerHTML = '';
+  finalData.forEach((series, index) => {
+    // const likeId = likes.findIndex((like) => Number(like.item_id) === index);
+    // let totalLikes;
+    // if (likeId >= 0) {
+    //   totalLikes = likes[likeId].likes;
+    // } else {
+    //   totalLikes = 0;
+    // }
+    const showCard = document.createElement('div');
+    showCard.classList.add('show-card');
+    const seriesImg = document.createElement('img');
+    seriesImg.classList.add('series-img');
+    seriesImg.src = series.image.original;
+    showCard.appendChild(seriesImg);
+    const seriesDetails = document.createElement('div');
+    seriesDetails.classList.add('series-details');
+    const seriesName = document.createElement('h3');
+    seriesName.classList.add('series-name');
+    seriesName.textContent = series.name;
+    seriesDetails.appendChild(seriesName);
+    const likeBtnDiv = document.createElement('div');
+    likeBtnDiv.classList.add('like-wrapper');
+    const likeIcon = document.createElement('i');
+    likeIcon.className = 'fa-solid fa-thumbs-up';
+    likeIcon.dataset.id = index;
+    likeBtnDiv.appendChild(likeIcon);
+    seriesDetails.appendChild(likeBtnDiv);
+    showCard.appendChild(seriesDetails);
+    // const likeCount = document.createElement("p");
+    // likeCount.classList.add("like-count");
+    // likeCount.textContent = totalLikes;
+    // showCard.appendChild(likeCount);
+    // const commentBtn = document.createElement("button");
+    // commentBtn.classList.add("comments");
+    // commentBtn.dataset.id = index;
+    // commentBtn.textContent = "Comment";
+    // showCard.appendChild(commentBtn);
+    seriesCardWrapper.appendChild(showCard);
+
+    // Event listener for the like button
+    // likeIcon.addEventListener("click", async (e) => {
+    //   await likePostApi(likeUrl, e.target.dataset.id); // Wait for like to be posted
+    //   totalLikes += 1; // Increment the like count
+    //   likeCount.textContent = totalLikes; // Update the like count immediately
+    // });
+  });
+};
+
+export default seriesCard;
